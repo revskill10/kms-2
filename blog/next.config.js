dbff48b4347e618bbc08359b7f1b79070720bf2e
Dung Truong <checkraiser11@gmail.com>
2018-12-06 20:34:59 +0700

    Fix next/link

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   //useFileSystemPublicRoutes: false,
-  assetPrefix: '/blog', // affects page bundles and app/commons/vendor scripts
+  //assetPrefix: '/blog', // affects page bundles and app/commons/vendor scripts
   webpack: (config) => {
-    config.output.publicPath = `/blog${config.output.publicPath}`;
+    //config.output.publicPath = `/blog${config.output.publicPath}`;
     return config;
   },
 };
