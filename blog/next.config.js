c8619345795db3672a198b70d3dac50971c0514f
Dung Truong <checkraiser11@gmail.com>
2018-12-06 17:17:04 +0700

    Fix prefix

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  assetPrefix: '/blog', // affects page bundles and app/commons/vendor scripts
+  //assetPrefix: '/blog', // affects page bundles and app/commons/vendor scripts
   webpack: (config) => {
     //config.output.publicPath = `/assets${config.output.publicPath}`;
     return config;
